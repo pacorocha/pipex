4a248246b632aadcc3430985621815eb38afe445
Paco Rocha <frocha@f-rocha.com>
2021-10-31 16:31:37 +0100

    feeding 2 files

--- src/mandatory/pipex.c
+++ src/mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 03:14:57 by jfrancis          #+#    #+#             */
-/*   Updated: 2021/10/30 19:35:09 by coder            ###   ########.fr       */
+/*   Updated: 2021/10/31 03:28:24 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int main(int argc, char **argv)
 		print_error(2);
 
 	pipeline.infile = try_open_file(argv[1], O_RDONLY);
+	pipeline.outfile = try_open_file(argv[4], O_CREAT | O_RDWR | O_TRUNC);
 	close(pipeline.infile);
 	return (0);
 }
